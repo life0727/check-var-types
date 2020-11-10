403ae87cf95b2ed58602a7ad12ef519818ca6c54
liiffe <mmlife0727@gmail.com>
2020-11-10 10:31:31 +0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,12 @@
 const path = require('path')
 
 module.exports = {
-    //entry: './src/main.js', //入口文件，src下的index.js
-    //entry: ["@babel/polyfill",'./src/main.js'], //入口文件，src下的index.js
-    entry: ['./src/main.js'], //入口文件，src下的index.js
+    entry: ['./src/main.js'], 
     output: {
-        path: path.join(__dirname, '/'), // 出口目录，dist文件
-        filename: 'index.js' //这里name就是打包出来的文件名，因为是单入口，就是main，多入口下回分解
+        path: path.join(__dirname, '/'), 
+        filename: 'index.js',
+        library:'check-var-types',
+        libraryTarget:'umd'
     },
     module: {
         rules:[
